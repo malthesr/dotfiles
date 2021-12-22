a345e66d1214d525ac7c58f1ab2b43e0c5d3b384
Malthe Sebro Rasmussen <malthesebro@protonmail.com>
2021-12-22 18:29:16 +0100

    Add basic fish setup

---
+++ .config/omf/init.fish
@@ -0,0 +1,23 @@
+### THEME CONFIG ###
+
+set -xg pure_enable_single_line_prompt true
+
+### ALIASES ###
+
+# Vi mode
+fish_vi_key_bindings
+
+# Dot files
+alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+alias dotfst='dotfiles status'
+alias dotfc='dotfiles commit'
+alias dotfa='dotfiles add'
+
+# General aliases
+function hx --wraps helix
+    helix $argv
+end
+
+function dotf --wraps git
+    git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $argv
+end
---
+++ .config/omf/key_bindings.fish
@@ -0,0 +1,1 @@
+bind -M insert \cf accept-autosuggestion
