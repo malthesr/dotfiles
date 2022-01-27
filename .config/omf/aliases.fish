deb1a0d91a15157e8f4a159027c82b4ccd752e6d
Malthe Sebro Rasmussen <malthesebro@protonmail.com>
2022-01-27 12:25:53 +0100

    fish: Split aliases into separate file

---
+++ .config/omf/aliases.fish
@@ -0,0 +1,31 @@
+function dotf --wraps git
+    git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $argv
+end
+
+function z --wraps zellij
+    zellij $argv
+end
+
+function l --wraps exa
+    exa $argv
+end
+    
+function c --wraps bat
+    bat $argv
+end
+
+function md --wraps mkdir
+    mkdir -p $argv
+end
+
+function take
+    mkdir -p $argv[1] && cd $argv[1]
+end
+
+function pg --wraps less
+    less -S $argv
+end
+
+function snk --wraps snakemake
+    nice snakemake -p $argv
+end
--- .config/omf/init.fish
+++ .config/omf/init.fish
@@ -1,5 +1,9 @@
 ### GENERAL ###
 
+# Source aliases
+set aliasconfig $OMF_CONFIG/aliases.fish
+source $aliasconfig
+
 # Source machine-specific config
 
 set localconfig $OMF_CONFIG/local.fish
@@ -23,38 +27,3 @@ set -xg pure_enable_single_line_prompt true
 set -xg pure_color_success $pure_color_primary
 set -xg pure_color_command_duration $pure_color_primary
 set -xg pure_show_jobs true
-
-### ALIASES ###
-
-# General aliases
-function dotf --wraps git
-    git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $argv
-end
-
-function z --wraps zellij
-    zellij $argv
-end
-
-function l --wraps exa
-    exa $argv
-end
-    
-function c --wraps bat
-    bat $argv
-end
-
-function md --wraps mkdir
-    mkdir -p $argv
-end
-
-function take
-    mkdir -p $argv[1] && cd $argv[1]
-end
-
-function pg --wraps less
-    less -S $argv
-end
-
-function snk --wraps snakemake
-    nice snakemake -p $argv
-end
