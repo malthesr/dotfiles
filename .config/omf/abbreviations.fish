c6a25595d3abce3dd24c05ab05e7e3fda5def279
Malthe Sebro Rasmussen <malthesebro@protonmail.com>
2022-03-17 13:11:25 +0100

    fish: Prefix 'python3 -m' for 'snk' abbreviation

--- .config/omf/abbreviations.fish
+++ .config/omf/abbreviations.fish
@@ -7,7 +7,7 @@ abbr --add pg 'less -S'
 abbr --add o 'xdg-open'
 abbr --add py 'python3 -q'
 abbr --add R 'R --quiet --no-save'
-abbr --add snk 'snakemake -p'
+abbr --add snk 'python3 -m snakemake -p'
 abbr --add xcp 'xclip -selection clipboard -in'
 
 if command -v exa > /dev/null
