3ea728aba4c42b7de8c0c54006a1846d9bb43261
Malthe Sebro Rasmussen <malthesebro@protonmail.com>
2025-07-08 11:41:31 +0200

    fish: Remove tmux start

--- .config/fish/conf.d/tmux.fish
+++
@@ -1,4 +0,0 @@
-# Run tmux if it exists, shell is interactive, and not already in tmux session
-if type -q tmux && status --is-interactive && not string match -rq tmux $TERM
-    tmux new-session
-end
